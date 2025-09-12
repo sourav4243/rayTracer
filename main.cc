647caebe4d20f476fbc7d4b46af9db2e8d14d3ac
sourav4243 <souravkumar18835@gmail.com>
2025-09-13 01:52:15 +0530

    use color.h in main.cc

--- main.cc
+++ main.cc
@@ -1,5 +1,7 @@
+#include "color.h"
+#include "vec3.h"
+
 #include<iostream>
-using namespace std;
 
 int main(){
     // Image
@@ -9,22 +11,15 @@ int main(){
 
     // Render
 
-    cout<< "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout<< "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for(int j=0; j<image_height; j++){
-        clog<<"\rScanlines remaining: "<< (image_height-j) << ' ' << flush; // progress bar
+        std::clog<<"\rScanlines remaining: "<< (image_height-j) << ' ' << std::flush; // progress bar
         for(int i=0; i<image_width; i++){
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999*r);
-            int ig = int(255.999*g);
-            int ib = int(255.999*b);
-
-            cout<< ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
 
-    clog << "\rDone.                \n";    // progress bar
+    std::clog << "\rDone.                \n";    // progress bar
 }
