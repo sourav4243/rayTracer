94030981f269241dfae70082bf565fe55b276636
sourav <souravkumar18835@gmail.com>
2025-10-04 00:55:42 +0530

    make normals oppose ray direction

--- hittable.h
+++ hittable.h
@@ -8,6 +8,21 @@ class hit_record {
         point3 p;   // hit point in 3D place
         vec3 normal;  // surface normal at hit point
         double t;   // ray eqn: p(t) = origin + t*direction 
+        bool front_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal){
+            // Sets the hit record normal vector.
+            // Note: the parameter 'outward_normal' is assumed to have unit length.
+
+            // dot product of ray direction and normal tells weather they are in same direction or not
+            // Ensure normal always opposes ray direction. 
+            // If ray is outside -> keep outward_normal.
+            // If ray is inside  -> flip it. Also mark front_face.
+
+
+            front_face = dot(r.direction(), outward_normal) < 0;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
 };
 
 
