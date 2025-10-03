1c9fface4c3f9d5d1982d9c1cac9e25204450070
sourav <souravkumar18835@gmail.com>
2025-10-03 10:09:38 +0530

    comments

--- main.cc
+++ main.cc
@@ -23,11 +23,12 @@ color ray_color(const ray& r){
     // rendering surface normals on a sphere as color
     if(t > 0.0){
         vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        // N ranges from -1 to 1, so add 1 to make it positive and then divide by 2 to scale down it between 0 and 1 ( as rgb color accepts [0, 1])
         return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 0.4) + a*color(0.5, 0.7, 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main(){
