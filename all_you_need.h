e142e1c870c33dc163e655a9a61d8b5942cd0e7d
sourav <souravkumar18835@gmail.com>
2025-10-05 02:00:13 +0530

    updated common header file

--- all_you_need.h
+++ all_you_need.h
@@ -11,7 +11,7 @@ using std:: make_shared;
 using std:: shared_ptr;
 
 // Constants 
-const double infinity = std:: numeric_limits<double> :: infinity();
+const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility function
