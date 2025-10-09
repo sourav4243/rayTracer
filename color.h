e1d12af1327794ca80cbe7d4fcf3492392d82168
sourav <souravkumar18835@gmail.com>
2025-10-09 10:56:07 +0530

    clamp color to {0, 255}

--- color.h
+++ color.h
@@ -1,6 +1,7 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 
 using color = vec3;
@@ -10,10 +11,11 @@ void write_color(std:: ostream& out, const color& pixel_color){
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
+    static const interval intensity(0.000, 0.999);
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // Write out the pixel color components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
