560e6b014682281fa93cf0ddfe769a75da44ab66
sourav <souravkumar18835@gmail.com>
2025-10-11 23:44:23 +0530

    undo remove of shadow acne

--- camera.h
+++ camera.h
@@ -91,7 +91,7 @@ class camera {
             hit_record rec;
 
             // behind the hood, iterate through world hittable_list, call hittable of each object, then color that
-            if(world.hit(r, interval(0.001, infinity), rec)){
+            if(world.hit(r, interval(0, infinity), rec)){
                 vec3 direction = random_on_hemisphere(rec.normal);
                 return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);   // using random ray direction for diffusion and 0.5 is used to half the brightness on each bounce
             }
