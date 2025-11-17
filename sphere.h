d070d9764c98860fb1477a645a5d602f201ede96
sourav <souravkumar18835@gmail.com>
2025-11-17 13:48:29 +0530

    initialize sphere with a material

--- sphere.h
+++ sphere.h
@@ -10,9 +10,7 @@ class sphere: public hittable {
         shared_ptr<material>mat;
 
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std:: fmax(0, radius)){
-            // TODO: Initialize the material pointer 'mat'
-        }
+        sphere(const point3& center, double radius, shared_ptr<material>mat) : center(center), radius(std:: fmax(0, radius)), mat(mat) {}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             vec3 oc = center - r.origin();
