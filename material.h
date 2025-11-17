2b8e3e43bf72389cf49e92204f7dbb18854141e9
sourav <souravkumar18835@gmail.com>
2025-11-17 13:35:26 +0530

    add lambertian (diffuse) class to material.h

--- material.h
+++ material.h
@@ -17,4 +17,25 @@ class material {
         }
 };
 
+class lambertian : public material {
+    private:
+        color albedo;
+
+    public: 
+        lambertian(const color& albedo) : albedo(albedo) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+        const override {
+            auto scatter_direction = rec.normal + random_unit_vector();
+
+            // Catch degenerate scatter direction
+            if (scatter_direction.near_zero())
+                scatter_direction = rec.normal;
+            
+            scattered = ray(rec.p, scatter_direction);
+            attenuation = albedo;
+            return true;
+        }
+};
+
 #endif
