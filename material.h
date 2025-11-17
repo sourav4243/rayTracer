b9fc10f15dafa09aad08b65671300baac9b78df6
sourav <souravkumar18835@gmail.com>
2025-11-17 13:44:51 +0530

    add metal class to material.h

--- material.h
+++ material.h
@@ -38,4 +38,18 @@ class lambertian : public material {
         }
 };
 
+class metal : public material {
+    private:
+        color albedo;
+    public:
+        metal(const color& albedo) : albedo(albedo) {}
+
+        bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+        const override {
+            vec3 reflected = reflect(r_in.direction(), rec.normal);
+            scattered = ray(rec.p, reflected);
+            attenuation = albedo;
+            return true;
+        }
+};
 #endif
