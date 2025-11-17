9c42056a4c232cfb2922c06a1935016809ab1768
sourav <souravkumar18835@gmail.com>
2025-11-17 13:32:07 +0530

    add material pointer in hit_record

--- hittable.h
+++ hittable.h
@@ -1,10 +1,13 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+class material;
+
 class hit_record {
     public:
         point3 p;   // hit point in 3D place
         vec3 normal;  // surface normal at hit point
+        shared_ptr<material>mat;
         double t;   // ray eqn: p(t) = origin + t*direction 
         bool front_face;
 
