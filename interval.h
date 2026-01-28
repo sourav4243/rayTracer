ccbfbc935015b8d0c893a120bfd3eb8be8eb2fab
sourav <souravkumar18835@gmail.com>
2026-01-29 00:43:51 +0530

    feat(interval): add constructor to merge two intervals

--- interval.h
+++ interval.h
@@ -9,6 +9,12 @@ class interval {
 
         interval(double min, double max) : min(min) , max(max) {}
         
+        interval(const interval& a, const interval& b) {
+            // Create the interval tightly enclosing the two input interval.
+            min = (a.min <= b.min) ? a.min : b.min;
+            max = (a.max >= b.max) ? a.max : b.max;
+        }
+
         double size() const {
             return max - min;
         }
