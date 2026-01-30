242126da7d35468a2c6358de430b024bf988fd47
sourav <souravkumar18835@gmail.com>
2026-01-30 22:33:25 +0530

    feat(bvh): add sorting comparator

--- bvh.h
+++ bvh.h
@@ -31,6 +31,7 @@ class bvh_node : public hittable {
                 left = objects[start];
                 right = objects[start+1];
             } else {
+                // This sorts only the subrange [start, end). Objects outside the node are untouched
                 std::sort(std::begin(objects) + start, std::begin(objects) + end, comparator);
 
                 auto mid = start + object_span/2;
@@ -38,6 +39,7 @@ class bvh_node : public hittable {
                 right = make_shared<bvh_node>(objects, mid, end);
             }
 
+            // Parent box fully encloses both children (OlogN traversal)
             bbox = aabb(left->bounding_box(), right->bounding_box());
         }
 
@@ -57,6 +59,26 @@ class bvh_node : public hittable {
         shared_ptr<hittable> left;
         shared_ptr<hittable> right;
         aabb bbox;
+
+    static bool box_compare(
+        const shared_ptr<hittable> a, const shared_ptr<hittable> b, int axis_index
+    ) {
+        auto a_axis_interval = a->bounding_box().axis_interval(axis_index);
+        auto b_axis_interval = b->bounding_box().axis_interval(axis_index);
+        return a_axis_interval.min < b_axis_interval.min;
+    }
+
+    static bool box_x_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 0);
+    }
+
+    static bool box_y_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 1);
+    }
+
+    static bool box_z_compare (const shared_ptr<hittable> a, const shared_ptr<hittable> b) {
+        return box_compare(a, b, 2);
+    }
 };
 
 #endif
