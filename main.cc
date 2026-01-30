c7b1b5670993754682381286fce03c7d2456c61f
sourav <souravkumar18835@gmail.com>
2026-01-30 22:37:25 +0530

    feat(main): include bvh.h and update world initialization with bvh_node

--- main.cc
+++ main.cc
@@ -1,4 +1,6 @@
 #include "all_you_need.h"
+
+#include "bvh.h"
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
@@ -47,6 +49,7 @@ int main(){
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
+    world = hittable_list(make_shared<bvh_node>(world));
 
     camera cam;
     cam.aspect_ratio      = 16.0 / 9.0;
